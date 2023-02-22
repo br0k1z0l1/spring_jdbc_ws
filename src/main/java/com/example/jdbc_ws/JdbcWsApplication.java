6e023582112e235d730af8a1c2ab16a3718254da
T.Sanyi <s.truczkai@gmail.com>
2023-02-22 09:38:13 +0100

    add jdbctemplate solution

--- src/main/java/com/example/jdbc_ws/JdbcWsApplication.java
+++ src/main/java/com/example/jdbc_ws/JdbcWsApplication.java
@@ -1,7 +1,13 @@
 package com.example.jdbc_ws;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
 public class JdbcWsApplication {
@@ -10,4 +16,28 @@ public class JdbcWsApplication {
         SpringApplication.run(JdbcWsApplication.class, args);
     }
 
+}
+@Component
+class CountryPrinter implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "select code, countryname, in_eu from country";
+        List<Country> countries = jdbcTemplate.query(
+                sql,
+                (rs, rowNum) -> new Country(
+                        rs.getString("code"),
+                        rs.getString("countryname"),
+                        rs.getBoolean("in_eu")
+                )
+        );
+
+        countries.forEach(System.out::println);
+    }
+}
+
+record Country(String code, String countryName, boolean inEu) {
 }
